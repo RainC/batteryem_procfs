596876ce08f459c3bf933f06f36d667c6ab5d4a9
DongGyu Kim <support@rainclab.net>
2018-06-02 22:07:35 +0900

    wefew
    Signed-off-by: DongGyu Kim <support@rainclab.net>

--- new/procfs_ex/procfs.c
+++ new/procfs_ex/procfs.c
@@ -1,381 +1,202 @@
-#include <linux/sched.h>
-#include <linux/rcupdate.h>
-#include <linux/module.h>   /* Needed by all modules */
-#include <linux/kernel.h>   /* Needed for KERN_INFO */
-#include <linux/proc_fs.h>
-#include <linux/string.h>
-#include <linux/vmalloc.h>
-#include <asm/uaccess.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/init.h>
-#include <asm/siginfo.h>
-#include <linux/signal.h>
+#include <linux/proc_fs.h>
 #include <linux/sched.h>
-#include <linux/rcupdate.h>
-#include <linux/string.h>
- 
-MODULE_LICENSE("GPL");
- 
- 
-#define PROCFS_MAX_SIZE         1024
-#define PROCFS_TESTLEVEL        "battery_test"
-#define PROCFS_NOTIFYPID        "battery_notify"
-#define PROCFS_THRESHOLD        "battery_threshold"
-#define PROCFS_PIDTH            "pid_th"
-#define PROCFS_LENGTH 8
- 
-/* Declaration of variables used in this module */
- 
- 
-static int level = 99;
-static int test_level = 0;                      //indicates level of battery remain.
-static int notify_pid = -1;
-static int threshold = -1;
- 
-/* End of declaration */
- 
-struct pid_th_t
-{
-	char pid[PROCFS_LENGTH + 1];
-	char threshold[PROCFS_LENGTH + 1];
+#include <asm/uaccess.h>
+
+
+#define MODULE_VERSION "1.0"
+#define MODULE_NAME "procfs_example"
+
+#define FOOBAR_LEN 8
+
+struct fb_data_t {
+        char name[FOOBAR_LEN + 1];
+        char value[FOOBAR_LEN + 1];
 };
 
-struct proc_dir_entry *proc_pid_th;
 
-struct pid_th_t foo_data;       
- 
-/* Declaration of ancillary variables */
- 
-static char procfs_buffer[PROCFS_MAX_SIZE];     
-static unsigned long procfs_buffer_size = 0;    //size of receive side buffer
-
-static char pidnum_buffer[PROCFS_MAX_SIZE];     
-static unsigned long pidnum_buffer_size = 0;    //size of receive side buffer
-
-static char threshold_buffer[PROCFS_MAX_SIZE];     
-static unsigned long threshold_buffer_size = 0;    //size of receive side buffer
-
-static struct proc_dir_entry *proc_entry;       //indicates procfs entry.
-static struct proc_dir_entry *pidnum_entry;       //pidnum, threshold
-static struct proc_dir_entry *threshold_entry;       //pidnum, threshold
+static struct proc_dir_entry *example_dir, *foo_file,
+        *bar_file, *jiffies_file, *tty_device, *symlink;
 
 
- 
-/* End of declaration */
- 
- 
- 
-/*
-        Implementation of procfs write function
-*/
-static int test_level_write( struct file *filp, const char *user_space_buffer, unsigned long len, loff_t *off )
+struct fb_data_t foo_data, bar_data;
+
+
+static int proc_read_jiffies(char *page, char **start,
+                             off_t off, int count,
+                             int *eof, void *data)
 {
- 
-        int status = 0;
-        int requested;
- 
-        procfs_buffer_size = len;
- 
-        if (procfs_buffer_size > PROCFS_MAX_SIZE ) {
-                procfs_buffer_size = PROCFS_MAX_SIZE;
-        }
- 
-        /* write data to the buffer */
-        if ( copy_from_user(procfs_buffer, user_space_buffer, procfs_buffer_size) ) {
-                return -EFAULT;
-        }
- 
-        status  = kstrtoint(procfs_buffer, 10, &requested);
-        printk(KERN_INFO "user_space buffer %c\n", user_space_buffer);
-        if(status < 0)
-        {
-                printk(KERN_INFO "Error while called kstrtoint(...) - %d", status);
-               
-               
-                return -ENOMEM;
-        } else {
-                printk(KERN_INFO "status > 0 -> success \n");
-        }
-        // validate level value.
-        if(requested< 0 || requested > 100){
-                printk(KERN_INFO "Invalid battery level.\n");
-                return -ENOMEM;
-        } else {
-                printk(KERN_INFO "request value success\n");
-        }
-        // accept value.
-        test_level = requested;
- 
-        // *off += procfs_buffer_size; // not necessary here!
-         int i;
-         for (i = 0; i < 1024 ; i ++) {
-                procfs_buffer[i] = (char)0;
-        }
+        int len;
 
-        return procfs_buffer_size; 
+        MOD_INC_USE_COUNT;
+        
+        len = sprintf(page, "jiffies = %ld\n",
+                      jiffies);
+
+        MOD_DEC_USE_COUNT;
+
+        return len;
 }
 
-static int test_level_read( struct file *filp, char *user_space_buffer, size_t count, loff_t *off )
+
+static int proc_read_foobar(char *page, char **start,
+                            off_t off, int count, 
+                            int *eof, void *data)
 {
-        int ret = 0;
-        int flag = 0;
- 
-        if(*off < 0) *off = 0;
- 
-        snprintf(procfs_buffer, 16, "%d\n", test_level);
+        int len;
+        struct fb_data_t *fb_data = (struct fb_data_t *)data;
+
+        MOD_INC_USE_COUNT;
         
-        procfs_buffer_size = strlen(procfs_buffer);
- 
-        if(*off > procfs_buffer_size){
-                return -EFAULT;
-        }else if(*off == procfs_buffer_size){
-                return 0;
-        }
- 
-        if(procfs_buffer_size - *off > count)
-                ret = count;
+        len = sprintf(page, "%s = '%s'\n", 
+                      fb_data->name, fb_data->value);
+
+        MOD_DEC_USE_COUNT;
+
+        return len;
+}
+
+
+static int proc_write_foobar(struct file *file,
+                             const char *buffer,
+                             unsigned long count, 
+                             void *data)
+{
+        int len;
+        struct fb_data_t *fb_data = (struct fb_data_t *)data;
+
+        MOD_INC_USE_COUNT;
+
+        if(count > FOOBAR_LEN)
+                len = FOOBAR_LEN;
         else
-                ret = procfs_buffer_size - *off;
-        flag = copy_to_user(user_space_buffer, procfs_buffer + (*off), ret);
- 
-        if(flag < 0)
+                len = count;
+
+        if(copy_from_user(fb_data->value, buffer, len)) {
+                MOD_DEC_USE_COUNT;
                 return -EFAULT;
-        
-        *off += ret;
- 
-        return ret;
- 
+        }
+
+        fb_data->value[len] = '\0';
+
+        MOD_DEC_USE_COUNT;
+
+        return len;
 }
-static int pidnum_write( struct file *filp, const char *user_space_buffer, unsigned long len, loff_t *off )
+
+
+static int __init init_procfs_example(void)
 {
- 
-        int status = 0;
-        int requested;
- 
-        pidnum_buffer_size = len;
- 
-        if (pidnum_buffer_size > PROCFS_MAX_SIZE ) {
-                pidnum_buffer_size = PROCFS_MAX_SIZE;
+        int rv = 0;
+
+        /* create directory */
+        example_dir = proc_mkdir(MODULE_NAME, NULL);
+        if(example_dir == NULL) {
+                rv = -ENOMEM;
+                goto out;
         }
- 
-        /* write data to the buffer */
-        if ( copy_from_user(pidnum_buffer, user_space_buffer, pidnum_buffer_size) ) {
-                return -EFAULT;
-        }
- 
-        status  = kstrtoint(pidnum_buffer, 10, &requested);
         
-        if(status < 0)
-        {
-                printk(KERN_INFO "Error while called kstrtoint(...) - %d", status);
+        example_dir->owner = THIS_MODULE;
+        
+        /* create jiffies using convenience function */
+        jiffies_file = proc_create("jiffies", 
+                                              0444, NULL, 
+                                              example_dir
+                                              );
+                                              
+        // proc_entry = proc_create_data(PROCFS_TESTLEVEL, 0666, NULL, &my_proc_fops, msg);
+        
+        if(jiffies_file == NULL) {
+                rv  = -ENOMEM;
+                goto no_jiffies;
+        }
+
+        jiffies_file->owner = THIS_MODULE;
+
+        /* create foo and bar files using same callback
+         * functions 
+         */
+        foo_file = create_proc_entry("foo", 0644, example_dir);
+        if(foo_file == NULL) {
+                rv = -ENOMEM;
+                goto no_foo;
+        }
+
+        strcpy(foo_data.name, "foo");
+        strcpy(foo_data.value, "foo");
+        foo_file->data = &foo_data;
+        foo_file->read_proc = proc_read_foobar;
+        foo_file->write_proc = proc_write_foobar;
+        foo_file->owner = THIS_MODULE;
                 
+        bar_file = create_proc_entry("bar", 0644, example_dir);
+        if(bar_file == NULL) {
+                rv = -ENOMEM;
+                goto no_bar;
+        }
+
+        strcpy(bar_data.name, "bar");
+        strcpy(bar_data.value, "bar");
+        bar_file->data = &bar_data;
+        bar_file->read_proc = proc_read_foobar;
+        bar_file->write_proc = proc_write_foobar;
+        bar_file->owner = THIS_MODULE;
                 
-                
-                
-                return -ENOMEM;
-        } else {
-                printk(KERN_INFO "status > 0 -> success \n");
+        /* create tty device */
+        tty_device = proc_mknod("tty", S_IFCHR | 0666,
+                                example_dir, MKDEV(5, 0));
+        if(tty_device == NULL) {
+                rv = -ENOMEM;
+                goto no_tty;
         }
-        // validate pidnum  value
-        /// 
-
-        if(requested< 0 || requested > 100){
-                printk(KERN_INFO "Invalid battery level.\n");
-                return -ENOMEM;
-        } else {
-                printk(KERN_INFO "request value success\n");
-        }
-        // accept value.
-        // threshold, notify pid detection..
-
-        notify_pid = requested;
         
-        int i;
-        // *off += pidnum_buffer_size; // not necessary here!
- 
-                for (i = 0; i < 1024 ; i ++) {
-                        pidnum_buffer[i] = (char)0;
-                }
-        return pidnum_buffer_size; 
+        tty_device->owner = THIS_MODULE;
+
+        /* create symlink */
+        symlink = proc_symlink("jiffies_too", example_dir, 
+                               "jiffies");
+        if(symlink == NULL) {
+                rv = -ENOMEM;
+                goto no_symlink;
+        }
+
+        symlink->owner = THIS_MODULE;
+
+        /* everything OK */
+        printk(KERN_INFO "%s %s initialised\n",
+               MODULE_NAME, MODULE_VERSION);
+        return 0;
+
+no_symlink:
+        remove_proc_entry("tty", example_dir);
+no_tty:
+        remove_proc_entry("bar", example_dir);
+no_bar:
+        remove_proc_entry("foo", example_dir);
+no_foo:
+        remove_proc_entry("jiffies", example_dir);
+no_jiffies:                           
+        remove_proc_entry(MODULE_NAME, NULL);
+out:
+        return rv;
 }
 
 
-static int pidnum_read( struct file *filp, char *user_space_buffer, size_t count, loff_t *off )
+static void __exit cleanup_procfs_example(void)
 {
-        int ret = 0;
-        int flag = 0;
- 
-        if(*off < 0) *off = 0;
- 
-        snprintf(pidnum_buffer, 16, "%d\n", test_level);
-        pidnum_buffer_size = strlen(pidnum_buffer);
- 
-        if(*off > pidnum_buffer_size){
-                return -EFAULT;
-        }else if(*off == pidnum_buffer_size){
-                return 0;
-        }
- 
-        if(pidnum_buffer_size - *off > count)
-                ret = count;
-        else
-                ret = pidnum_buffer_size - *off;
-        flag = copy_to_user(user_space_buffer, pidnum_buffer + (*off), ret);
- 
-        if(flag < 0)
-                return -EFAULT;
-        
-        *off += ret;
- 
-        return ret;
- 
-}
+        remove_proc_entry("jiffies_too", example_dir);
+        remove_proc_entry("tty", example_dir);
+        remove_proc_entry("bar", example_dir);
+        remove_proc_entry("foo", example_dir);
+        remove_proc_entry("jiffies", example_dir);
+        remove_proc_entry(MODULE_NAME, NULL);
 
-static int threshold_write( struct file *filp, const char *user_space_buffer, unsigned long len, loff_t *off )
-{
- 
-        int status = 0;
-        int requested;
- 
-        threshold_buffer_size = len;
- 
-        if (threshold_buffer_size > PROCFS_MAX_SIZE ) {
-                threshold_buffer_size = PROCFS_MAX_SIZE;
-        }
- 
-        /* write data to the buffer */
-        if ( copy_from_user(threshold_buffer, user_space_buffer, threshold_buffer_size) ) {
-                return -EFAULT;
-        }
- 
-        status  = kstrtoint(threshold_buffer, 10, &requested);
-        
-        if(status < 0)
-        {
-                printk(KERN_INFO "Error while called kstrtoint(...)\n");
-                return -ENOMEM;
-        } else {
-                printk(KERN_INFO "status > 0 -> success \n");
-        }
-        // validate pid_th  value
-        /// 
-
-        if(requested< 0 || requested > 100){
-                printk(KERN_INFO "Invalid battery level.\n");
-                return -ENOMEM;
-        } else {
-                printk(KERN_INFO "request value success\n");
-        }
-        // accept value.
-        // threshold, notify pid detection..
-
-        notify_pid = requested;
-        
-
-        // *off += threshold_buffer_size; // not necessary here!
- 
-        return threshold_buffer_size; 
+        printk(KERN_INFO "%s %s removed\n",
+               MODULE_NAME, MODULE_VERSION);
 }
 
 
-static int threshold_read( struct file *filp, char *user_space_buffer, size_t count, loff_t *off )
-{
-        int ret = 0;
-        int flag = 0;
- 
-        if(*off < 0) *off = 0;
- 
-        snprintf(threshold_buffer, 16, "%d\n", test_level);
-        threshold_buffer_size = strlen(threshold_buffer);
- 
-        if(*off > threshold_buffer_size){
-                return -EFAULT;
-        }else if(*off == threshold_buffer_size){
-                return 0;
-        }
- 
-        if(threshold_buffer_size - *off > count)
-                ret = count;
-        else
-                ret = threshold_buffer_size - *off;
-        flag = copy_to_user(user_space_buffer, threshold_buffer + (*off), ret);
- 
-        if(flag < 0)
-                return -EFAULT;
-        
-        *off += ret;
- 
-        return ret;
- 
-}
-
-
-
-
-
- 
-/*
-        Implementation of procfs read function
-*/
-
-
- 
- 
-/*
-        Configuration of file_operations
- 
-        This structure indicate functions when read or write operation occured.
-*/
-static const struct file_operations my_proc_fops = {
-        .write = test_level_write,
-        .read = test_level_read,
-};
-
-static const struct file_operations pidnum_ops = {
-        .write = pidnum_write,
-        .read = pidnum_read,
-};
-
-static const struct file_operations threshold_ops = {
-        .write = threshold_write,
-        .read = threshold_read,
-};
-
- 
-
- 
- 
-/*
-        This function will be called on initialization of  kernel module
-*/
-int init_process(void)
-{
-        int ret = 0;
-        char *msg;
-        msg="123 123";
-
-        proc_entry = proc_create_data(PROCFS_TESTLEVEL, 0666, NULL, &my_proc_fops, msg);
-        pidnum_entry = proc_create_data("pidnum" ,0666, NULL,&pidnum_ops, msg);
-
-        printk(KERN_ALERT "[init] init!!");
-
-        if(proc_entry == NULL && pidnum_entry == NULL && threshold_entry == NULL)
-        {
-                printk(KERN_ALERT "[error] pid_th_entry&other is failed");
-                return -ENOMEM;
-        }
-        return ret;
- 
-}
- 
-/*
-        This function will be called on cleaning up of kernel module
-*/
-void process_exit(void)
-{
-        printk(KERN_ALERT "[exit]Exit");
-        remove_proc_entry(PROCFS_TESTLEVEL, proc_entry);
-        remove_proc_entry("pidnum", pidnum_entry);
-}
-
-module_init(init_process);
-module_exit(process_exit);
+module_init(init_procfs_example);
+module_exit(cleanup_procfs_example);
