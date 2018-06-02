6443466c8f708433794f7bdcfbfc6502d1a4a81c
DongGyu Kim <support@rainclab.net>
2018-06-02 20:08:37 +0900

    pid, threshold entry added
    Signed-off-by: DongGyu Kim <support@rainclab.net>

--- new/procfs_ex/procfs.c
+++ new/procfs_ex/procfs.c
@@ -41,15 +41,24 @@ struct pid_th_t
 
 struct proc_dir_entry *proc_pid_th;
 
-struct pid_th_t foo_data;
+struct pid_th_t foo_data;       
  
 /* Declaration of ancillary variables */
  
 static char procfs_buffer[PROCFS_MAX_SIZE];     
 static unsigned long procfs_buffer_size = 0;    //size of receive side buffer
 
+static char pidnum_buffer[PROCFS_MAX_SIZE];     
+static unsigned long pidnum_buffer_size = 0;    //size of receive side buffer
+
+static char threshold_buffer[PROCFS_MAX_SIZE];     
+static unsigned long threshold_buffer_size = 0;    //size of receive side buffer
+
 static struct proc_dir_entry *proc_entry;       //indicates procfs entry.
-static struct proc_dir_entry *pid_th_entry;       //indicates procfs entry.
+static struct proc_dir_entry *pidnum_entry;       //pidnum, threshold
+static struct proc_dir_entry *threshold_entry;       //pidnum, threshold
+
+
  
 /* End of declaration */
  
@@ -96,13 +105,9 @@ static int test_level_write( struct file *filp, const char *user_space_buffer, u
  
         // *off += procfs_buffer_size; // not necessary here!
  
-        return procfs_buffer_size;
- 
+        return procfs_buffer_size; 
 }
- 
-/*
-        Implementation of procfs read function
-*/
+
 static int test_level_read( struct file *filp, char *user_space_buffer, size_t count, loff_t *off )
 {
         int ret = 0;
@@ -111,6 +116,7 @@ static int test_level_read( struct file *filp, char *user_space_buffer, size_t c
         if(*off < 0) *off = 0;
  
         snprintf(procfs_buffer, 16, "%d\n", test_level);
+        
         procfs_buffer_size = strlen(procfs_buffer);
  
         if(*off > procfs_buffer_size){
@@ -127,12 +133,178 @@ static int test_level_read( struct file *filp, char *user_space_buffer, size_t c
  
         if(flag < 0)
                 return -EFAULT;
- 
+        
         *off += ret;
  
         return ret;
  
 }
+static int pidnum_write( struct file *filp, const char *user_space_buffer, unsigned long len, loff_t *off )
+{
+ 
+        int status = 0;
+        int requested;
+ 
+        pidnum_buffer_size = len;
+ 
+        if (pidnum_buffer_size > PROCFS_MAX_SIZE ) {
+                pidnum_buffer_size = PROCFS_MAX_SIZE;
+        }
+ 
+        /* write data to the buffer */
+        if ( copy_from_user(pidnum_buffer, user_space_buffer, pidnum_buffer_size) ) {
+                return -EFAULT;
+        }
+ 
+        status  = kstrtoint(pidnum_buffer, 10, &requested);
+        
+        if(status < 0)
+        {
+                printk(KERN_INFO "Error while called kstrtoint(...)\n");
+                return -ENOMEM;
+        } else {
+                printk(KERN_INFO "status > 0 -> success \n");
+        }
+        // validate pidnum  value
+        /// 
+
+        if(requested< 0 || requested > 100){
+                printk(KERN_INFO "Invalid battery level.\n");
+                return -ENOMEM;
+        } else {
+                printk(KERN_INFO "request value success\n");
+        }
+        // accept value.
+        // threshold, notify pid detection..
+
+        notify_pid = requested;
+        
+
+        // *off += pidnum_buffer_size; // not necessary here!
+ 
+        return pidnum_buffer_size; 
+}
+
+
+static int pidnum_read( struct file *filp, char *user_space_buffer, size_t count, loff_t *off )
+{
+        int ret = 0;
+        int flag = 0;
+ 
+        if(*off < 0) *off = 0;
+ 
+        snprintf(pidnum_buffer, 16, "%d\n", test_level);
+        pidnum_buffer_size = strlen(pidnum_buffer);
+ 
+        if(*off > pidnum_buffer_size){
+                return -EFAULT;
+        }else if(*off == pidnum_buffer_size){
+                return 0;
+        }
+ 
+        if(pidnum_buffer_size - *off > count)
+                ret = count;
+        else
+                ret = pidnum_buffer_size - *off;
+        flag = copy_to_user(user_space_buffer, pidnum_buffer + (*off), ret);
+ 
+        if(flag < 0)
+                return -EFAULT;
+        
+        *off += ret;
+ 
+        return ret;
+ 
+}
+
+static int threshold_write( struct file *filp, const char *user_space_buffer, unsigned long len, loff_t *off )
+{
+ 
+        int status = 0;
+        int requested;
+ 
+        threshold_buffer_size = len;
+ 
+        if (threshold_buffer_size > PROCFS_MAX_SIZE ) {
+                threshold_buffer_size = PROCFS_MAX_SIZE;
+        }
+ 
+        /* write data to the buffer */
+        if ( copy_from_user(threshold_buffer, user_space_buffer, threshold_buffer_size) ) {
+                return -EFAULT;
+        }
+ 
+        status  = kstrtoint(threshold_buffer, 10, &requested);
+        
+        if(status < 0)
+        {
+                printk(KERN_INFO "Error while called kstrtoint(...)\n");
+                return -ENOMEM;
+        } else {
+                printk(KERN_INFO "status > 0 -> success \n");
+        }
+        // validate pid_th  value
+        /// 
+
+        if(requested< 0 || requested > 100){
+                printk(KERN_INFO "Invalid battery level.\n");
+                return -ENOMEM;
+        } else {
+                printk(KERN_INFO "request value success\n");
+        }
+        // accept value.
+        // threshold, notify pid detection..
+
+        notify_pid = requested;
+        
+
+        // *off += threshold_buffer_size; // not necessary here!
+ 
+        return threshold_buffer_size; 
+}
+
+
+static int threshold_read( struct file *filp, char *user_space_buffer, size_t count, loff_t *off )
+{
+        int ret = 0;
+        int flag = 0;
+ 
+        if(*off < 0) *off = 0;
+ 
+        snprintf(threshold_buffer, 16, "%d\n", test_level);
+        threshold_buffer_size = strlen(threshold_buffer);
+ 
+        if(*off > threshold_buffer_size){
+                return -EFAULT;
+        }else if(*off == threshold_buffer_size){
+                return 0;
+        }
+ 
+        if(threshold_buffer_size - *off > count)
+                ret = count;
+        else
+                ret = threshold_buffer_size - *off;
+        flag = copy_to_user(user_space_buffer, threshold_buffer + (*off), ret);
+ 
+        if(flag < 0)
+                return -EFAULT;
+        
+        *off += ret;
+ 
+        return ret;
+ 
+}
+
+
+
+
+
+ 
+/*
+        Implementation of procfs read function
+*/
+
+
  
  
 /*
@@ -144,7 +316,17 @@ static const struct file_operations my_proc_fops = {
         .write = test_level_write,
         .read = test_level_read,
 };
- 
+
+static const struct file_operations pidnum_ops = {
+        .write = pidnum_write,
+        .read = pidnum_read,
+};
+
+static const struct file_operations threshold_ops = {
+        .write = threshold_write,
+        .read = threshold_read,
+};
+
  
 
  
@@ -156,16 +338,18 @@ int init_process(void)
 {
         int ret = 0;
         char *msg;
-        msg="123123";
+        msg="123 123";
+
         proc_entry = proc_create(PROCFS_TESTLEVEL, 0666, NULL, &my_proc_fops);
-        printk(KERN_ALERT "[init] pid_th writing..");
-        pid_th_entry = proc_create_data("pid_th" ,0666,NULL,&my_proc_fops,msg);
+        pidnum_entry = proc_create("pid_th" ,0666, NULL,&pidnum_ops);
+        threshold_entry = proc_create("threshold", 0666, NULL, &threshold_ops)
+        
 
         printk(KERN_ALERT "[init] init!!");
 
-        if(proc_entry == NULL && pid_th_entry == NULL)
+        if(proc_entry == NULL && pid_th_entry == NULL && threshold_entry == NULL)
         {
-                printk(KERN_ALERT "[error] pid_th_entry&other null");
+                printk(KERN_ALERT "[error] pid_th_entry&other is failed");
                 return -ENOMEM;
         }
         return ret;
