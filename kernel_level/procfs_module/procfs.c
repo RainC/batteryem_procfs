fead1492c685735936e10db4624f376eaf5246b4
DongGyu Kim <support@rainclab.net>
2018-06-03 05:25:07 +0900

    정리 포기
    Signed-off-by: DongGyu Kim <support@rainclab.net>

--- kernel_level/procfs_module/procfs.c
+++ kernel_level/procfs_module/procfs.c
@@ -35,12 +35,9 @@ MODULE_LICENSE("GPL");
  
  
 static int level = 99;
-int test_level = 0;                      //indicates level of battery remain.
+static int test_level = 0;                      //indicates level of battery remain.
 static int notify_pid = -1;
 static int threshold = -1;
-
-
-EXPORT_SYMBOL(test_level)
 
 /* End of declaration */
  
